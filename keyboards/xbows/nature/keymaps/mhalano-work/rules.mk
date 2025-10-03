cab12cc0e819efab2cb741b1ad8fa5bcb34cf281
Marcos Alano <marcoshalano@gmail.com>
2025-10-03 14:54:06 -0300

    Revert "Remove MOUSE_SHARED_EP"
    This reverts commit 5c2946f0affaa6f76cb7da7a0a1b92574d1f69fb.

--- keyboards/xbows/nature/keymaps/mhalano-work/rules.mk
+++ keyboards/xbows/nature/keymaps/mhalano-work/rules.mk
@@ -4,3 +4,4 @@ RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = is31fl3731
 MOUSEKEY_ENABLE = no # My Windows machine keeps waking up from suspend because of this, so I disabled it for now.
 USB_WAIT_FOR_ENUMERATION = yes
+MOUSE_SHARED_EP = no
