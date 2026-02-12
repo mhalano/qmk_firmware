31b125dd4f3cab959b11af8b15c20ad10bc80e22
Marcos Alano <marcoshalano@gmail.com>
2026-02-12 12:04:10 -0300

    Remove mhalano-work since Im using a Knight Plus, not a Nature

--- keyboards/xbows/nature/keymaps/mhalano-work/rules.mk
+++
@@ -1,7 +0,0 @@
-TAP_DANCE_ENABLE = yes
-CAPS_WORD_ENABLE  = yes
-RGB_MATRIX_ENABLE = yes
-RGB_MATRIX_DRIVER = is31fl3731
-MOUSEKEY_ENABLE = no # My Windows machine keeps waking up from suspend because of this, so I disabled it for now.
-USB_WAIT_FOR_ENUMERATION = yes
-MOUSE_SHARED_EP = no
